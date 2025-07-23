86ff79d537a5271aed6fda435dce8545dd60e5d9
fsociety <fsociety@kali>
2025-07-23 04:06:28 +0100

    PUSH : data

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <stack>
+template <typename T> class MutantStack : public std::stack<T>
+{
+    private:
+        T* _memory;
+    public:
+    // template <typename ITER>
+    class iterator
+    {
+        private:
+            T* ptr;
+        public:
+            iterator(T*_ptr) : ptr(_ptr)
+            {}
+            iterator& operator=(const iterator &other)
+            {
+                this->ptr = other.ptr;
+                return (*this);
+            }
+            T& operator*()
+            {
+                return (*ptr);
+            }
+            T& operator[](int index)
+            {
+                return (ptr[index]);
+            }
+            T *operator->()
+            {
+                return (ptr);
+            }
+            iterator& operator++()
+            {
+                ptr++;
+                return (*this);
+            }
+            iterator& operator--()
+            {
+                ptr--;
+                return (*this);
+            }
+            iterator& operator++(int)
+            {
+                iterator tmp = *this;
+                ++ptr;
+                return (tmp);
+            }
+            iterator& operator--(int)
+            {
+                iterator tmp = *this;
+                --ptr;
+                return (tmp);
+            }
+            bool operator==(const iterator& op)
+            {
+                return (this->ptr == op.ptr);
+            }
+            bool operator!=(const iterator& op)
+            {
+                return (this->ptr != op.ptr);
+            }
+    };
+    iterator begin()
+    {
+        return (&this->c[0]);
+    }
+    iterator end()
+    {
+        return (&this->c[0] + this->c.size());
+    }
+        
+
+};
+template <typename T> std::ostream& operator<<(std::ostream& out, typename MutantStack<T>::iterator& it)
+{
+    out << *it;
+    return (out);
+}
---
+++ cpp08/ex02/main.cpp
@@ -0,0 +1,22 @@
+#include "MutantStack.hpp"
+
+int main()
+{
+    MutantStack<int> stack;
+    stack.push(6);
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    stack.push(4);
+    stack.push(5);
+    stack.push(6);
+    MutantStack<int>::iterator it = stack.begin();
+    MutantStack<int>::iterator ite = stack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+}
